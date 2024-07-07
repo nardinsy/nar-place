52fcda0f9c63e2a979b86f6fb03a2a0c92438adc
nardinsy <nasaya174@gmail.com>
2024-07-07 16:06:32 +0330

    test: upload image

--- frontend/src/shared/ImageUpload.tsx
+++ frontend/src/shared/ImageUpload.tsx
@@ -18,21 +18,25 @@ const ImageUpload: FC<ImageUploadT> = ({
   const pickImageHandler = (event: ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
     const files = (event.target as HTMLInputElement).files;
-    if (files && files.length !== 0) {
-      const file: File = files[0];
-      onChangeImage(file);
+    if (!files || files.length === 0) {
+      return;
     }
+
+    const file: File = files[0];
+    onChangeImage(file);
   };
 
   const triggerFileChangeHandler = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     if (!inputRef.current) throw new Error("Can not upload image succesfully");
+
     inputRef.current.click();
   };
 
   return (
     <div>
       <input
+        data-testid="photo-uploader"
         id={id}
         ref={inputRef}
         style={{ display: "none" }}
@@ -40,7 +44,11 @@ const ImageUpload: FC<ImageUploadT> = ({
         accept=".jpg,.png,.jpeg"
         onChange={pickImageHandler}
       />
-      <button onClick={triggerFileChangeHandler} className={className}>
+      <button
+        data-testid="upload-button"
+        onClick={triggerFileChangeHandler}
+        className={className}
+      >
         {children}
       </button>
     </div>
