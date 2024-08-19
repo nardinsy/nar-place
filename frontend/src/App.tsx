33510098827f227e4b45fcd95d0b61c8c38eba4f
nardinsy <nasaya174@gmail.com>
2024-08-19 14:45:02 +0330

    TRANSITION TO TAILWIND DONE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Route } from "react-router-dom";
 import MainHeader from "./Header/MainHeader";
 import AuthForms from "./Authentication/AuthForm";
