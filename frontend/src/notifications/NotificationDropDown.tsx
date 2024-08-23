73a3a21f735064953af07e1399be7f4abcf511a8
nardinsy <nasaya174@gmail.com>
2024-08-23 14:37:06 +0330

    Modified notification drop down bullet point for new notifications

--- frontend/src/notifications/NotificationDropDown.tsx
+++ frontend/src/notifications/NotificationDropDown.tsx
@@ -17,37 +17,46 @@ const NotificationDropdown: FC<NotificationDropdownT> = ({}) => {
   const ref = useRef<HTMLDivElement | null>(null);
 
   const generateNotificationsItem = (
-    notifications: NotificationDto[] | undefined
+    notifications: NotificationDto[] | undefined,
+    status: "OLD" | "NEW"
   ) => {
     if (!notifications) return <></>;
     return notifications.map((notification: NotificationDto, index: number) => {
       return (
         <li
           key={index}
-          className="cursor-context-menu px-4 py-6 rounded-xl mb-1 hover:bg-edit-button-bg transition-colors ease-in duration-[0.1s]"
+          className="relative cursor-context-menu flex flex-row items-center px-4 py-6 rounded-xl mb-1 hover:bg-edit-button-bg transition-colors ease-in duration-[0.1s]"
         >
           <NotificationItem notificationDto={notification} />
+          {status === "NEW" && (
+            <span
+              className={`absolute right-0 w-2 h-2 rounded bg-primary-hover mx-2`}
+            />
+          )}
         </li>
       );
     });
   };
 
   const oldNotifications = generateNotificationsItem(
-    authCtx.readOldNotificationsFromLocalStorage()
+    authCtx.readOldNotificationsFromLocalStorage(),
+    "OLD"
   );
 
-  const newNotifications = generateNotificationsItem(notifCtx.newNotifications);
+  const newNotifications = generateNotificationsItem(
+    notifCtx.newNotifications,
+    "NEW"
+  );
 
   return (
     <div
-      className="absolute top-16 right-5 bg-white w-16rem md:w-[21rem] mb-24 shadow-default rounded-xl overflow-hidden animate-slide-down"
+      className="absolute top-16 right-3 bg-white w-[21rem] md:w-[25rem] mb-24 shadow-default rounded-xl overflow-hidden animate-slide-down"
       data-testid="drop"
       ref={ref}
     >
       <div className="font-bold px-6 pt-4 tracking-wide">Notifications</div>
       <ul className="my-3 flex flex-col px-2 max-h-72 overflow-x-hidden overflow-y-scroll scroll-my-13">
         {newNotifications}
-        <li key={"a special key"}>-----------------------</li>
         {oldNotifications}
       </ul>
     </div>
